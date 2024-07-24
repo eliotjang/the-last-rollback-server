6828f9019ec3cea3678c904056ec6f3c79ae0049
mimihimesama <gwa445@naver.com>
2024-07-24 17:15:37 +0900

    🐛 fix : playerInfo 전달 수정

--- src/handlers/town/enter-town.handler.js
+++ src/handlers/town/enter-town.handler.js
@@ -5,17 +5,19 @@ import { serialize } from '../../utils/packet-serializer.utils.js';
 import TransformInfo from '../../protobuf/classes/info/transform-info.proto.js';
 import StatInfo from '../../protobuf/classes/info/stat-info.proto.js';
 import PlayerInfo from '../../protobuf/classes/info/player-info.proto.js';
+import { playerInfoToObject } from '../../utils/transform-object.utils.js';
 
 const enterTownHandler = async ({ socket, userId, packet }) => {
   try {
+    const playerId = Math.floor(Math.random() * 10) + 1;
     const transform = new TransformInfo(0, 0, 0, 0);
     const statInfo = new StatInfo(1, 100);
-    let playerId = 1;
     const playerInfo = new PlayerInfo(playerId, packet.nickname, packet.class, transform, statInfo);
 
     // await createUser(userId, nickname, characterClass, transform.posX, transform.posY);
 
-    const user = { playerInfo, socket };
+    const plainPlayerInfo = playerInfoToObject(playerInfo);
+    const user = { playerInfo: plainPlayerInfo, socket };
 
     const townSessions = getAllTownSessions();
     let townSession = townSessions.find((townSession) => !townSession.isFull());
@@ -25,7 +27,7 @@ const enterTownHandler = async ({ socket, userId, packet }) => {
 
     townSession.addUser(user);
 
-    const response = serialize(packetTypes.S_ENTER, { player: playerInfo });
+    const response = serialize(packetTypes.S_ENTER, { player: plainPlayerInfo });
     socket.write(response);
   } catch (error) {
     handleError(socket, error);
