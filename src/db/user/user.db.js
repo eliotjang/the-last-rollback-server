a1a7f10baf9c9f1527c1649a5f1768b817e0ef36
mimihimesama <gwa445@naver.com>
2024-07-25 15:14:18 +0900

    🐛 fix : 캐릭터 스폰 수정 중

--- src/db/user/user.db.js
+++ src/db/user/user.db.js
@@ -21,13 +21,17 @@ export const updateLastPosition = async (x, y, accountId) => {
   await pools.USER_DB.query(SQL_QUERIES.UPDATE_LAST_POSITION, [x, y, accountId]);
 };
 
-export const updateClass = async (accountClass, accountId) => {
-  await pools.USER_DB.query(SQL_QUERIES.UPDATE_CLASS, [accountClass, accountId]);
+export const updateUserInfo = async (nickname, accountClass, accountId) => {
+  await pools.USER_DB.query(SQL_QUERIES.UPDATE_USER_INFO, [nickname, accountClass, accountId]);
 };
 
-export const updateNickname = async (nickname, accountId) => {
-  await pools.USER_DB.query(SQL_QUERIES.UPDATE_NICKNAME, [nickname, accountId]);
-};
+// export const updateClass = async (accountClass, accountId) => {
+//   await pools.USER_DB.query(SQL_QUERIES.UPDATE_CLASS, [accountClass, accountId]);
+// };
+
+// export const updateNickname = async (nickname, accountId) => {
+//   await pools.USER_DB.query(SQL_QUERIES.UPDATE_NICKNAME, [nickname, accountId]);
+// };
 
 export const updateLevel = async (level, accountId) => {
   await pools.USER_DB.query(SQL_QUERIES.UPDATE_LEVEL, [level, accountId]);
--- src/db/user/user.queries.js
+++ src/db/user/user.queries.js
@@ -3,8 +3,9 @@ export const SQL_QUERIES = {
   CREATE_USER: 'INSERT INTO user (account_id, account_pwd) VALUES (?, ?)',
   UPDATE_USER_LOGIN: 'UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE account_id = ?',
   UPDATE_LAST_POSITION: `UPDATE user SET last_position_x = ?, last_position_y = ? WHERE account_id = ?`,
-  UPDATE_CLASS: `UPDATE user SET account_class = ? WHERE account_id = ?`,
-  UPDATE_NICKNAME: `UPDATE user SET account_nickname = ? WHERE account_id = ?`,
+  UPDATE_USER_INFO: `UPDATE user SET account_nickname = ?, account_class = ? WHERE account_id = ?`,
+  // UPDATE_CLASS: `UPDATE user SET account_class = ? WHERE account_id = ?`,
+  // UPDATE_NICKNAME: `UPDATE user SET account_nickname = ? WHERE account_id = ?`,
   UPDATE_LEVEL: `UPDATE user SET account_level = ? WHERE account_id = ?`,
   UPDATE_EXPERIENCE: `UPDATE user SET account_experience = ? WHERE account_id = ?`,
 };
