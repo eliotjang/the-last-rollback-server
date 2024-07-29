7f5ced1f2ea6c2fd179787520044c241d993533b
mimihimesama <gwa445@naver.com>
2024-07-29 21:07:43 +0900

    🧪 fix : 매칭 이후 세션 처리 수정 중

--- src/match_queue/match_queue.js
+++ src/match_queue/match_queue.js
@@ -1,4 +1,8 @@
 import { MAX_USERS } from '../constants/game.constants.js';
+import { townRedis } from './town.redis.js';
+import { dungeonRedis } from './dungeon.redis.js';
+import { getTownSessionByUserId } from '../session/town.session.js';
+import { payloadTypes } from '../../constants/packet.constants.js';
 
 // const waitingList = []; // one list per stage?
 const waitingLists = {}; // dungeonCode : waitingList[];
@@ -20,7 +24,7 @@ export const addToWaitingList = (data) => {
  *
  * @param {number} dungeonCode
  */
-export const checkWaitingList = (dungeonCode) => {
+export const checkWaitingList = async (dungeonCode) => {
   console.log('------------');
   if (waitingLists[dungeonCode].length >= MAX_USERS) {
     const users = waitingLists.splice(0, MAX_USERS);
@@ -29,11 +33,25 @@ export const checkWaitingList = (dungeonCode) => {
     // TODO: 위의 users에 있는 유저들을 포함하는 게임 세션 생성
 
     // BattleSession 생성
+    const hostId = users[0];
+    const hostInfo = await townRedis.getPlayerInfo(hostId);
+    await dungeonRedis.createDungeon(hostInfo, hostId);
 
     // users의 유저를 battle session에 추가
+    for (const user of users) {
+      const playerInfo = await townRedis.getPlayerInfo(user);
+      await dungeonRedis.createGuest(playerInfo, hostId, user);
+    }
 
     // town session에서 유저 제거
+    for (const user of users) {
+      await townRedis.removePlayer(user, false);
+    }
 
     // 각 유저의 town session 내 다른 유저에게 Despawn 패킷 전송
+    for (const user of users) {
+      const townSession = getTownSessionByUserId(user); // 이 방법으로 townSession에 저장했다는 가정 하에
+      townSession.notifyOthers(user, payloadTypes.S_DESPAWN, { playerIds: users });
+    }
   }
 };
