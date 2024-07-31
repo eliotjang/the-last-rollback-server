0a06f1bb5b3239fb88cbba8f4df9e6c925bafe69
HeY1996 <37572040+HyuneonY@users.noreply.github.com>
2024-07-31 19:25:09 +0900

    Fix 플레이어 이동 동기화

--- src/handlers/battle/enter-battle.handler.js
+++ src/handlers/battle/enter-battle.handler.js
@@ -41,3 +41,17 @@ const enterBattleHandler = ({ socket, userId, payload }) => {
 };
 
 export default enterBattleHandler;
+
+function selectionSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  }
+  return arr;
+}
--- src/handlers/game/locationUpdate.handler.js
+++ src/handlers/game/locationUpdate.handler.js
@@ -1,31 +1,43 @@
-import { packetTypes } from '../../constants/packet.constants.js';
+import { payloadTypes } from '../../constants/packet.constants.js';
 import { getTownSessionByUserSocket } from '../../session/town.session.js';
+import { getBattleSessionByUserSocket } from '../../session/battle.session.js';
 import CustomError from '../../utils/error/customError.js';
 import { ErrorCodes } from '../../utils/error/errorCodes.js';
 import { handleError } from '../../utils/error/errorHandler.js';
+import { getUserById } from '../../session/user.session.js';
 //import { updatePosition } from '../../classes/models/user.class.js';
 
 const locationUpdateHandler = ({ socket, userId, packet }) => {
   try {
     const { transform } = packet;
-    const townSession = getTownSessionByUserSocket(socket);
+    console.log('transform', transform);
+    // const currentSession = getUserById(socket).sessionInfo.type;
+    // console.log('######', currentSession);
+    let playerSession = getTownSessionByUserSocket(socket);
+    // if (currentSession === 1) {
+    //   // 1: TOWN
+    //   playerSession = getTownSessionByUserSocket(socket);
+    // } else if (currentSession === 2) {
+    //   // 2: BATTLE
+    //   playerSession = getBattleSessionByUserSocket(socket);
+    // } else {
+    //   throw new CustomError(ErrorCodes.GAME_NOT_FOUND, '잘못된 세션 입니다.');
+    // }
 
-    if (!townSession) {
-      throw new CustomError(ErrorCodes.GAME_NOT_FOUND, '타운 세션을 찾을 수 없습니다.');
+    if (!playerSession) {
+      throw new CustomError(ErrorCodes.GAME_NOT_FOUND, '현재 세션을 찾을 수 없습니다.');
     }
 
-    const user = townSession.getUserBySocket(socket);
+    const user = playerSession.getUserBySocket(socket);
     if (!user) {
       throw new CustomError(ErrorCodes.USER_NOT_FOUND, '유저를 찾을 수 없습니다.');
     }
 
     //user.updatePosition(TransformInfo);
-    user.playerInfo.transform = transform;
-    const data = { playerId: user.playerInfo.playerId, transform };
+    user.getPlayerInfo().transform = transform;
+    const payload = { session: 1, transform };
 
-    //const TransformInfos = townSession.getAllLocation(user.playerInfo.playerId);
-
-    townSession.sendPacketToAll(packetTypes.S_MOVE, data);
+    playerSession.notifyOthers(user.accountId, payloadTypes.S_MOVE, payload);
   } catch (error) {
     handleError(socket, error);
   }
