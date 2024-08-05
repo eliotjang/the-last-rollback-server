9c299fe908066306a5773ef6a5d39b755401f9fe
Dongkyun Kim <donkim0122@gmail.com>
2024-08-06 00:41:18 +0900

    Update: 매치 큐 관련 constant 추가

--- src/constants/match.constants.js
+++ src/constants/match.constants.js
@@ -2,3 +2,4 @@ export const MATCH_QUEUE_NAME = 'match-queue';
 
 export const MATCH_ENQUEUE_JOB_NAME = 'match-enqueue';
 export const MATCH_DEQUEUE_JOB_NAME = 'match-dequeue';
+export const MATCH_QUEUE_RETRIEVAL = 'check-queue';
