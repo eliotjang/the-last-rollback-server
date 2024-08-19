6883ab0c5f34f09c8d449f2bc30539c55a20729d
mimihimesama <gwa445@naver.com>
2024-08-19 13:10:36 +0900

    ✨ update : bull에 redis 추가

--- src/bull/player/consumers/enter-town.consumers.js
+++ src/bull/player/consumers/enter-town.consumers.js
@@ -1,8 +1,14 @@
 import Bull from 'bull';
 import { ENTER_TOWN_JOB_NAME, ENTER_TOWN_QUEUE_NAME } from '../../../constants/constants.js';
 import EnterTownProcessor from '../processors/enter-town.processor.js';
+import { config } from '../../../config/config.js';
 
-const enterTownQueue = new Bull(ENTER_TOWN_QUEUE_NAME);
+const enterTownQueue = new Bull('ENTER_TOWN_QUEUE_NAME', {
+  redis: {
+    host: config.redis.redisHost,
+    port: config.redis.redisPort,
+  },
+});
 
 export const setEnterTownQueue = () => {
   enterTownQueue.process(ENTER_TOWN_JOB_NAME, EnterTownProcessor);
