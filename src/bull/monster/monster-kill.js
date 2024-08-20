a22b67682b4e8ca689301601c4b9ad86f9076212
mimihimesama <gwa445@naver.com>
2024-08-20 13:36:52 +0900

    🐛 fix : Bull에 redis 추가

--- src/bull/monster/monster-kill.js
+++ src/bull/monster/monster-kill.js
@@ -1,8 +1,14 @@
 import Bull from 'bull';
 import { MONSTER_HIT_JOB_NAME, MONSTER_HIT_QUEUE_NAME } from '../../constants/constants.js';
 import { setMonsterHitQueue } from './consumers/monster-hit.consumers.js';
+import { config } from '../../config/config.js';
 
-const monsterHitQueue = new Bull(MONSTER_HIT_QUEUE_NAME);
+const monsterHitQueue = new Bull('MONSTER_HIT_QUEUE_NAME', {
+  redis: {
+    host: config.redis.redisHost,
+    port: config.redis.redisPort,
+  },
+});
 setMonsterHitQueue();
 
 export const enqueueMonsterHitJob = (payloadData) => {
