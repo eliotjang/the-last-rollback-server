f29d7b71b64bbc99e618459b82cac49b4072c00d
mimihimesama <gwa445@naver.com>
2024-08-20 13:42:33 +0900

    🐛 fix : config 파일 위치 오류 수정

--- src/bull/monster/consumers/monster-hit.consumers.js
+++ src/bull/monster/consumers/monster-hit.consumers.js
@@ -1,7 +1,7 @@
 import Bull from 'bull';
 import { MONSTER_HIT_JOB_NAME, MONSTER_HIT_QUEUE_NAME } from '../../../constants/constants.js';
 import MonsterHitProcessor from '../processors/monster-hit.processor.js';
-import { config } from '../../config/config.js';
+import { config } from '../../../config/config.js';
 
 const monsterHitQueue = new Bull('MONSTER_HIT_QUEUE_NAME', {
   redis: {
