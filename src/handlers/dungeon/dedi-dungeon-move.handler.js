23cdb5ac00503a64739d0b4d1c7d1af6c3681090
Dongkyun Kim <donkim0122@gmail.com>
2024-08-21 11:17:02 +0900

    Update: 패킷 구조 추가/수정 Docs: comment 설명 추가

--- src/handlers/dungeon/dedi-dungeon-move.handler.js
+++ src/handlers/dungeon/dedi-dungeon-move.handler.js
@@ -1,7 +1,7 @@
 import DediClient from '../../classes/sessions/dedi-client.class.js';
 
 const dediDungeonMoveHandler = ({ socket, accountId, packet }) => {
-  //
+  // TODO: 클라에서 받은 정보를 토대로 dedi 서버에 C_SetPlayerDest 패킷 전송
   const { posX: x, posY: y, posZ: z } = packet.transformInfo;
   const dungeonSession = getDungeonSessionByUserSocket(socket);
   DediClient.getClient(dungeonSession.id).setPlayerDest(accountId, { x, y, z });
--- src/handlers/dungeon/dedi-monster-move.handler.js
+++ src/handlers/dungeon/dedi-monster-move.handler.js
@@ -1,10 +1,10 @@
 import DediClient from '../../classes/sessions/dedi-client.class.js';
 
 const dediMonsterMoveHandler = ({ socket, accountId, packet }) => {
-  //
-  //   const { posX: x, posY: y, posZ: z } = packet.transformInfo;
-  //   const dungeonSession = getDungeonSessionByUserSocket(socket);
-  //   DediClient.getClient(dungeonSession.id).setPlayerDest(accountId, { x, y, z });
+  // TODO: 클라에서 받은 정보를 토대로 DediClient의 setMonsterDest 메서드 호출
+  const { posX: x, posY: y, posZ: z } = packet.transformInfo;
+  const dungeonSession = getDungeonSessionByUserSocket(socket);
+  DediClient.getClient(dungeonSession.id).setMonsterDest(accountId, { x, y, z });
 };
 
 export default dediMonsterMoveHandler;
