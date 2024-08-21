e2c48664b868e780e26454a99e46b402cf00d278
mimihimesama <gwa445@naver.com>
2024-08-21 11:52:02 +0900

    🐛 fix : target payload 수정

--- src/handlers/dungeon/dedi-monster-move.handler.js
+++ src/handlers/dungeon/dedi-monster-move.handler.js
@@ -4,10 +4,16 @@ const dediMonsterMoveHandler = ({ socket, accountId, packet }) => {
   // TODO: 클라에서 받은 정보를 토대로 DediClient의 setMonsterDest 메서드 호출
   const { monsterIdx, accountId: player, structureIdx } = packet;
   const dungeonSession = getDungeonSessionByUserSocket(socket);
+
+  let target;
+
   if (structureIdx) {
-    DediClient.getClient(dungeonSession.id).setMonsterDest(monsterIdx);
+    target = { TargetStructure: { structureIdx } };
+  } else if (player) {
+    target = { targetPlayer: { accountId: player } };
   }
-  DediClient.getClient(dungeonSession.id).setMonsterDest(monsterIdx, player);
+
+  DediClient.getClient(dungeonSession.id).setMonsterDest(monsterIdx, target);
 };
 
 export default dediMonsterMoveHandler;
