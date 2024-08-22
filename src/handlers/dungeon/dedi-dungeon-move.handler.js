d8cf04a4163e2befde80afba00cb8dabde03d63f
eliotjang <37320831+eliotjang@users.noreply.github.com>
2024-08-23 02:10:45 +0900

    Fix 타운에서 스페이스바 클릭 오류

--- src/handlers/dungeon/dedi-dungeon-move.handler.js
+++ src/handlers/dungeon/dedi-dungeon-move.handler.js
@@ -1,21 +1,25 @@
 import DediClient from '../../classes/sessions/dedi-client.class.js';
+import { sessionTypes } from '../../constants/session.constants.js';
 import { getDungeonSessionByUserSocket } from '../../session/dungeon.session.js';
+import { getUserById } from '../../session/user.session.js';
 
 const dediDungeonMoveHandler = ({ socket, accountId, packet }) => {
+  const user = getUserById(accountId);
+  if (user.sessionInfo.type === sessionTypes.TOWN) {
+    return;
+  }
+
   if (!packet.transformInfo) {
     const dungeonSession = getDungeonSessionByUserSocket(socket);
-    if (!dungeonSession.id) {
-      console.log('던전 세션 정보 없음');
-      return;
-    }
-    DediClient.getClient(dungeonSession.id)?.setPlayerDest(accountId);
+
+    DediClient.getClient(dungeonSession.id).setPlayerDest(accountId);
     return;
   }
   // TODO: 클라에서 받은 정보를 토대로 dedi 서버에 C_SetPlayerDest 패킷 전송
   const { posX: x, posY: y, posZ: z } = packet.transformInfo;
 
   const dungeonSession = getDungeonSessionByUserSocket(socket);
-  DediClient.getClient(dungeonSession.id)?.setPlayerDest(accountId, { x, y, z });
+  DediClient.getClient(dungeonSession.id).setPlayerDest(accountId, { x, y, z });
 };
 
 export default dediDungeonMoveHandler;
